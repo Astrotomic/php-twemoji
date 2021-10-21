89231c857969db83185cd8c0e32e64904827c417
JoshyPHP <s9e.dev@gmail.com>
2021-10-21 14:53:42 +0200

    Tidied up code

--- src/EmojiText.php
+++ src/EmojiText.php
@@ -66,8 +66,6 @@ class EmojiText
 
     protected function regexp(): string
     {
-        //return '/(?:'.json_decode(file_get_contents(dirname(__FILE__).'/regexp.json')).')/u';
-        //return '/'.file_get_contents(__DIR__.'/regexp_utf8.txt').'/u';
-        return '/'.file_get_contents(__DIR__.'/regexp_bytes.txt').'/';
+        return '/'.file_get_contents(__DIR__.'/emoji_bytes.regexp').'/';
     }
 }
