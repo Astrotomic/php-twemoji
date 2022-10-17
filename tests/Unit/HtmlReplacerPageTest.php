b3a57bee3398474cd63a056cd3b3391b9b3c205c
Dan <self@danpock.me>
2022-10-17 13:35:17 -0400

    Add tests that cover the edge case I've been chasing

--- tests/Unit/HtmlReplacerPageTest.php
+++ tests/Unit/HtmlReplacerPageTest.php
@@ -43,12 +43,39 @@ HTML;
     assertMatchesTextSnapshot($results);
 });
 
-it('will replace the Emoji on page, but not in head', function () {
+it('will replace the Emoji on page, but not in head - OLD Charset Method', function () {
     $pageHtml = <<<'HTML'
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>HTML 5🚀 Boilerplate</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <h1>Do a quick kickflip! 🛹</h1>
+        <p>This is HTML text that should be replaced, but the emoji in the head should not.</p>
+        <h2>Time for a CRAB RAVE!</h2>
+        <p>🦀🦀🦀🦀🦀</p>
+        <p>🦀🦀🦀</p>
+        <p>🦀🦀🦀🦀🦀</p>
+        <h2>🙏🐘</h2>
+    </body>
+</html>
+HTML;
+    $results = htmlReplacerPngParser($pageHtml);
+    expect($results)->toContain('5🚀')->not()->toContain('&#');
+    assertMatchesTextSnapshot($results);
+})->skip('This will fail due to "incorrect" meta charset method, we need to consider how to address that.');
+
+it('will replace the Emoji on page, but not in head - NEW Charset Method', function () {
+    $pageHtml = <<<'HTML'
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    	<meta http-equiv="content-type" content="text/html; charset=utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>HTML 5🚀 Boilerplate</title>
