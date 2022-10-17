bc61a6aad36c7842fec97530952c5aab276a6cf7
Dan <self@danpock.me>
2022-10-17 12:18:54 -0400

    fix pest file styles

--- tests/Pest.php
+++ tests/Pest.php
@@ -5,5 +5,6 @@ use Astrotomic\Twemoji\HtmlReplacer;
 function htmlReplacerPngParser(string $html): string
 {
     $htmlReplacer = (new HtmlReplacer())->png();
+
     return $htmlReplacer->parse($html);
 }
