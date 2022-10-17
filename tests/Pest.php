e0f2540d67df9d0767b1672144e6d65f94abe560
Dan <self@danpock.me>
2022-10-17 12:18:09 -0400

    track the Pest helper file

---
+++ tests/Pest.php
@@ -0,0 +1,9 @@
+<?php
+
+use Astrotomic\Twemoji\HtmlReplacer;
+
+function htmlReplacerPngParser(string $html): string
+{
+    $htmlReplacer = (new HtmlReplacer())->png();
+    return $htmlReplacer->parse($html);
+}
